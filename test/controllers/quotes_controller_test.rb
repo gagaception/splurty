ea5d0dc38510ad4bcd2453e10621dfa16a124e33
Aziz <sharipov.aziz.sh@gmail.com>
2015-09-16 05:12:31 +0000

    Finished quote controller test

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "qoute show page" do
+     quote = Quote.create(:author => 'Aziz Sharipov', :saying => 'Everything is awesome' )
+     get :show, :id => quote.id
+     assert_response :success
+   end
+
+   test "quote show page, not found" do
+   	 get :show, :id => 'OMG'
+   	 assert_response :not_found
+   end
 end
