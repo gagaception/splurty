a1374b77281c01b79cb9cac9d958fee7ffb53d55
Aziz <sharipov.aziz.sh@gmail.com>
2015-09-16 05:38:52 +0000

    Added FactoryGirl gem

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
    test "qoute show page" do
-     quote = Quote.create(:author => 'Aziz Sharipov', :saying => 'Everything is awesome' )
+     quote = FactoryGirl.create(:quote)
      get :show, :id => quote.id
      assert_response :success
    end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
   test "unique_tag" do 
-  	quote = Quote.create(:author => 'Aziz Sharipov', :saying => 'Just start and you will succeed.')
+  	quote = FactoryGirl.create(:quote, :author => "Aziz Sharipov")
   	expected = 'AS#' + quote.id.to_s
   	actual = quote.unique_tag
   	assert_equal expected, actual
